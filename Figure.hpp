9f3f55772464bc6c80d1b6f270475e951cb2ac96
Snow <a.dolin125@gmail.com>
2021-03-22 19:01:11 +0200

    Add Figure.hpp

---
+++ Figure.hpp
@@ -0,0 +1,5 @@
+//
+// Created by Snow-PC on 22.03.2021.
+//
+
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+
+
+int main() {
+
+
+    return 0;
+}
