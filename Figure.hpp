950e29aa65709a087c04e3d968306c3b038e04f7
Snow <a.dolin125@gmail.com>
2021-03-24 17:28:01 +0200

    Update

--- Figure.hpp
+++ Figure.hpp
@@ -3,13 +3,29 @@
 #include <fstream>
 #include <iomanip>
 #include <algorithm>
+#include "Triangle.hpp"
+#include "Rectangle.hpp"
+#include "Circle.hpp"
+#include "Ellipse.hpp"
 
 // Figure - базовый класс, другие фигуры будут наследоваться от него
 // есть S - площадь, P - периметр
 // main() - взаимодействует с файлом input.txt, берет оттуда значения {x,y} для построения по координатам
 // нужновывести сумму всех площадей и периметров всех фигур
 
+
 class Figure {
+public:
+    Figure();
+    void Perimeter(uint32_t a) {
+
+    }
+
+
+private:
+    int a, b, c, d = 0;
+    uint32_t m_perimeter = 0;
+    uint32_t m_square = 0;
 
 };
 
